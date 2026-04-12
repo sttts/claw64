1390befa2060e92e87679b190b311f7de15a8e4e
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2026-04-12 10:48:02 +0200

    c64/layout: use wrapped tail advance in guarded staging

--- c64/guarded_queue.asm
+++ c64/guarded_queue.asm
@@ -34,8 +34,7 @@ guq_noop_store_body:
 guq_noop_copy_done:
         inc USERQ_STAGE_LEN
         inc USERQ_COUNT_PTR
-        inc USERQ_TAIL_PTR
-        inc USERQ_HEAD_PTR
+        jsr guard_userq_advance_tail
         rts
 
 guard_userq_enqueue_from_rxbuf:
